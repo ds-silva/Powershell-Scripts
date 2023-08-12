6981606d62d49daecb4139d476cd34c470c2fc95
David Silva <davidinteraction@outlook.com>
2023-08-12 09:12:28 -0300

    Adicionando tratamento de erro

--- clrearLogs.ps1
+++ clrearLogs.ps1
@@ -6,11 +6,10 @@ $retentionsDaysList = $retentionsDaysListStr.split(";");
 
 $loopExecutionTime = $logsPathList.length;
 
-for ($i=0; $i -ne $loopExecutionTime; $i++)
-{
+for ($i = 0; $i -ne $loopExecutionTime; $i++) {
   $pathsAndDates = @(
     $data = @(
-      [pscustomobject]@{path=$logsPathList[$i];date=$retentionsDaysList[$i]}
+      [pscustomobject]@{path = $logsPathList[$i]; date = $retentionsDaysList[$i] }
     )
   )
 
@@ -18,25 +17,35 @@ for ($i=0; $i -ne $loopExecutionTime; $i++)
     $path = $_.path;
     $date = $_.date;
 
-    $files = Get-ChildItem $path | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-$date) }
+    # Adding error handling around Get-ChildItem
+    try {
+      $files = Get-ChildItem $path | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-$date) }
+    }
+    catch {
+      Write-Output "Error occurred while getting files from $path: $_"
+      continue  # Move to the next iteration in case of an error
+    }
 
-    if ($files.Count -gt $date)
-    {
+    if ($files.Count -gt $date) {
       $cmd = "cd -d ""$path"""
       Write-Output "Exec: $cmd"
       Invoke-Expression $cmd
 
-      foreach ($file in $files)
-      {
+      foreach ($file in $files) {
         Write-Output $file.Name
       }
 
-      $cmd = "Get-ChildItem -Include *.log,*.txt,*.json -Recurse | Where-Object {(`$_.LastWriteTime -lt (Get-Date).AddDays(-$date))} | Remove-Item"
-      Write-Output "Exec: $cmd"
-      Invoke-Expression $cmd
+      # Adding error handling around Remove-Item
+      try {
+        $cmd = "Get-ChildItem -Include *.log,*.txt,*.json -Recurse | Where-Object {(`$_.LastWriteTime -lt (Get-Date).AddDays(-$date))} | Remove-Item"
+        Write-Output "Exec: $cmd"
+        Invoke-Expression $cmd
+      }
+      catch {
+        Write-Output "Error occurred while removing files: $_"
+      }
     }
-    else
-    {
+    else {
       Write-Output "No files found in $path that are more than $date days old."
     }
   }
